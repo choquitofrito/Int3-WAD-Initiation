3d0910cbc4f8f3eff617e49379f1c91745cad032
choquitofrito <zyriab@gmail.com>
2021-09-28 10:17:33 +0200

    exos

---
+++ Exercices/Enonces/04-Exercices First Class Fonctions.php
@@ -0,0 +1,10 @@
+# Exercices
+
+1. Créer une fonction anonyme qui calcule la somme de deux valeurs
+2. Créer une fonction anonyme qui calcule la multiplication de deux valeurs
+3. Créer un array contenant ces deux fonctions
+4. Faites appel à ces deux fonctions en utilisant l'array
+5. Faites une boucle qui parcourt l'array en faisant appel à chaque fonction dans l'array
+6. Rajoutez une nouvelle fonction dans l'array qui calcule la soustraction
+
+
