acb4bb53af0db0e94638860602e4c192e188a1d8
choquitofrito <zyriab@gmail.com>
2022-10-14 09:45:23 +0200

    ok

--- Notes/AjaxCours/06-FormulaireRechercheBD/main.js
+++ Notes/AjaxCours/06-FormulaireRechercheBD/main.js
@@ -1,17 +1,22 @@
 // événement pour le champ de recherche par titre
-document.getElementById ("txtTitre").addEventListener ("keyup", (event)=> {
+document.getElementById("txtTitre").addEventListener("keyup", (event) => {
     let val = event.target.value;
-    console.log (val);
+    console.log(val);
 
     let xhr = new XMLHttpRequest();
     let formulaire = new FormData(document.getElementById("formulaireHTML"));
-    
-    xhr.onreadystatechange = (event) => {
-        
 
+    xhr.onreadystatechange = (event) => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 304) {
+                console.log("tout ok");
+            }
+            else {
+                console.log("erreur: " + xhr.status);
+            }
+        }
     };
 
-
-    xhr.open ("POST", "./traitementRecherche.php");
-    xhr.send (formulaire);
+    xhr.open("POST", "./traitementRecherche.php");
+    xhr.send(formulaire);
 });
