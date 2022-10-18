68ba590d99ec34aa9220cee35b4be538820a12bb
choquitofrito <zyriab@gmail.com>
2022-10-18 13:44:01 +0200

    ok

--- Notes/BDCRUD/monSiteBD/sql/wadfilms.sql
+++ Notes/BDCRUD/monSiteBD/sql/wadfilms.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : ven. 30 sep. 2022 à 10:41
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.0.10
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2022 at 01:42 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `wadfilms`
+-- Database: `wadfilms`
 --
 CREATE DATABASE IF NOT EXISTS `wadfilms` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `wadfilms`;
@@ -26,7 +26,27 @@ USE `wadfilms`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `film`
+-- Table structure for table `favori`
+--
+
+CREATE TABLE `favori` (
+  `id` int(11) NOT NULL,
+  `idFilm` int(11) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `favori`
+--
+
+INSERT INTO `favori` (`id`, `idFilm`, `idUtilisateur`) VALUES
+(2, 8, 3),
+(3, 9, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
 --
 
 CREATE TABLE `film` (
@@ -35,28 +55,93 @@ CREATE TABLE `film` (
   `duree` int(11) NOT NULL,
   `description` text NOT NULL,
   `dateSortie` date NOT NULL,
-  `image` varchar(100) NOT NULL
+  `image` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Index pour les tables déchargées
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id`, `titre`, `duree`, `description`, `dateSortie`, `image`) VALUES
+(8, 'Matilda', 0, '', '0000-00-00', '634e5ceae51922022-10-18-09-59-38images.jpg'),
+(9, 'Up', 0, '', '0000-00-00', '634e61cf4530f2022-10-18-10-20-31Up_(2009_film).jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(200) NOT NULL,
+  `login` varchar(200) NOT NULL,
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `nom`, `login`, `password`) VALUES
+(3, 'wad', 'wad@interface3.be', '$2y$12$qmNA5LChmxFNnF1YrV/3quXujUJS5j9qYCcqKgl.848eKzgtTyQAm');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `film`
+-- Indexes for table `favori`
+--
+ALTER TABLE `favori`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idFilm` (`idFilm`),
+  ADD KEY `idUtilisateur` (`idUtilisateur`);
+
+--
+-- Indexes for table `film`
 --
 ALTER TABLE `film`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `film`
+-- AUTO_INCREMENT for table `favori`
+--
+ALTER TABLE `favori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `favori`
+--
+ALTER TABLE `favori`
+  ADD CONSTRAINT `favori_ibfk_1` FOREIGN KEY (`idFilm`) REFERENCES `film` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `favori_ibfk_2` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
