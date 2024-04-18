d7c6b0f367658fe7ed8b7a2372c873142bb61408
choquitofrito <zyriab@gmail.com>
2024-04-18 15:37:24 +0200

    tables in PHP

---
+++ CodeCoursWAD24/18-4-2024/tablesHTMLpays.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Capitale</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>Belgique</td>
+            <td>Bruxelles</td>
+        </tr>
+        <tr>
+            <td>France</td>
+            <td>Paris</td>
+        </tr>
+    </table>
+
+    <style>
+        table, tr, td {
+            border: 1px solid blue;
+        }
+    </style>
+
+</body>
+
+</html>
