39dffe37464e68e908e736999527d491b6711314
unknown <zyriab@gmail.com>
2025-06-30 14:18:34 +0200

    doc: base card

--- Temp/BDFormulaires-Musique/includes/contenu-accueil.php
+++ Temp/BDFormulaires-Musique/includes/contenu-accueil.php
@@ -16,17 +16,16 @@ $stmt->execute();
 // dans un array
 $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-print ('<div class="card" style="width: 18rem;">');
-print ('<img class="card-img-top" src="..." alt="Card image cap">');
-print ('<div class="card-body">');
-print ('<h5 class="card-title">Card title</h5>');
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-
+foreach ($res as $cle => $arrGroupe) {
+    print ('<div class="card" style="width: 18rem;">');
+    print ('<img class="card-img-top" src="./images/' . $arrGroupe['lienImage']. '" alt="Card image cap">');
+    print ('<div class="card-body">');
+    print ('<h5 class="card-title">' . $arrGroupe['nom'] . '</h5>');
+    print ('<p class="card-text">Description</p>');
+    print ('<a href="#" class="btn btn-primary">Go somewhere</a>');
+    print ('</div>');
+    print ('</div>');
+}
 
 var_dump ($res);
 
