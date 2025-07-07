15ce72bea6fa8ffa37f7dd1b26fad23a8f1e0d00
choquitofrito <choquitofrito>
2025-07-07 10:02:15 +0200

    form login added

---
+++ CodeCours/LoginPass/formInscription.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./formInscriptionTraitement.php" method="POST">
+        Nom<input type="text" name="nom"><br>
+        Email<input type="email" name="email"><br>
+        Password<input type="password" name="password"><br>
+        Ré-tapez Password<input type="password" name="re-password"><br>
+        <input type="submit" value="S'inscrire!">
+    </form>
+    
+</body>
+</html>
